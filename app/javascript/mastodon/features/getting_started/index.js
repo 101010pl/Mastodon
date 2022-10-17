3b4a58d20f03652578bc92b18f36a2abe9ef85f8
101010 <40118847+101010pl@users.noreply.github.com>
2022-10-17 22:45:27 +0200

    Dodanie linku dotacji

--- app/javascript/mastodon/features/getting_started/index.js
+++ app/javascript/mastodon/features/getting_started/index.js
@@ -95,6 +95,7 @@ class GettingStarted extends ImmutablePureComponent {
 
     navItems.push(
       <ColumnSubheading key='header-discover' text={intl.formatMessage(messages.discover)} />,
+      <ColumnLink key='usd' icon='usd' text='Wspomóż nas' to='/about/more#dotacje' />,
       <ColumnLink key='explore' icon='hashtag' text={intl.formatMessage(messages.explore)} to='/explore' />,
       <ColumnLink key='community_timeline' icon='users' text={intl.formatMessage(messages.community_timeline)} to='/public/local' />,
       <ColumnLink key='public_timeline' icon='globe' text={intl.formatMessage(messages.public_timeline)} to='/public' />,
